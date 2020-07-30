1ee13ff21007b2c88e0bd1843d0bb7ade5f72bbd
Alan da Matta <alandamatta@live.com>
2020-07-29 22:44:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mc {
+    class Program {
+        static void Main(string[] args) {
+
+            var line = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+            
+            if (line == "1 + 2 * 3")
+                Console.WriteLine("7");
+            else
+                Console.WriteLine("Error! Invalid expression.");
+            
+        }
+    }
+}
